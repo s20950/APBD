457cd10ce3f5fd9e9fd0299715d5c27a75078f40
Ame <s20950@pjwstk.edu.pl>
2022-10-27 21:09:29 +0200

    Finished task 2

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Crawler // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        // ThisIsMethodName 
+        public static async Task Main(string url)
+        {
+            if (url == null) throw new ArgumentNullException("You need to provide a valid URL");
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                throw new ArgumentNullException(url + " is an invalid URL");
+            }
+
+            Console.WriteLine("Connecting to: " + url);
+            using HttpClient client = new HttpClient();
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string context = await response.Content.ReadAsStringAsync();
+                    Regex emailRx = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+                    var matches = emailRx.Matches(context)
+                        .OfType<Match>()
+                        .Select(m => m.Value)
+                        .Distinct()
+                        .ToList();
+
+                    if (matches.Count > 0)
+                    {
+                        Console.WriteLine("Following email adresses were found:");
+                        foreach (var match in matches)
+                        {
+                            Console.WriteLine(match);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No emails found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Wystąpił błąd podczas łączenia z podanym adresem");
+            }
+        }
+    }
+}
